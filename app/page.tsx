05be1cdb5aa486450474768a5dc068d7c1247481
Mario Andres Hernandez Zapata <mahernandez9818@gmail.com>
2026-09-20 12:53:04 -0500

    feat: add HomeStats component to display vault statistics

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { HomeHero } from "@/components/home-hero";
+import { HomeStats } from "@/components/home-stats";
 import { ScrollReveal } from "@/components/scroll-reveal";
 
 export default function Home() {
   return (
     <div className="fade-in">
       <HomeHero />
+      <HomeStats />
       <ScrollReveal />
     </div>
   );
